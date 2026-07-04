837fb3f532939550aeb4b25a2ce50e9b315cca50
TheMonHub <mono@themonhub.net>
2026-07-04 12:30:29 +0700

    Actually added Mixin

--- src/main/java/io/github/teamfurrydevs/macas/mixin/ExampleMixin.java
+++ src/main/java/io/github/teamfurrydevs/macas/mixin/ExampleMixin.java
@@ -1,15 +1,23 @@
 package io.github.teamfurrydevs.macas.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
     @Inject(at = @At("HEAD"), method = "loadLevel")
     private void init(CallbackInfo info) {
         // This code is injected into the start of MinecraftServer.loadLevel()V
+        LOGGER.info("Initializing Example");
     }
 }
